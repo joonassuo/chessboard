b7e02fa088d687e2c1a6100898862709d99c6707
Joonas <joonas.suonpera@gmail.com>
2019-05-27 08:58:21 +0300

    working

--- functionality/start.js
+++ functionality/start.js
@@ -4,7 +4,6 @@ const whiteKnight ='background-image:url(https://upload.wikimedia.org/wikipedia/
 const whiteBishop ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png);'
 const whiteQueen ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/68px-Chess_qlt45.svg.png);'
 const whiteKing ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/68px-Chess_klt45.svg.png);'
-
 const blackPawn = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/800px-Chess_pdt45.svg.png);'
 const blackRook ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png);'
 const blackKnight ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png);'
@@ -18,36 +17,41 @@ function selectorFunction(id, piece) {
     document.querySelector(id).style.cssText = piece;
 }
 
+
+// populate board when startButton is clicked
 $('.startButton').click(() => {
-    // loop for all pawns
+    
+    // pawns
     for(let i = 97; i < 105; i++) {
         document.querySelector('#'+ String.fromCharCode(i) + 2).style.cssText = whitePawn;
         document.querySelector('#'+ String.fromCharCode(i) + 7).style.cssText = blackPawn;
     }
-
     // rooks
     selectorFunction('#a1', whiteRook);
     selectorFunction('#h1', whiteRook);
     selectorFunction('#a8', blackRook);
     selectorFunction('#h8', blackRook);
-
     // knights
     selectorFunction('#b1', whiteKnight);
     selectorFunction('#g1', whiteKnight);
     selectorFunction('#b8', blackKnight);
     selectorFunction('#g8', blackKnight);
-
     // bishops
     selectorFunction('#c1', whiteBishop);
     selectorFunction('#f1', whiteBishop);
     selectorFunction('#c8', blackBishop);
     selectorFunction('#f8', blackBishop);
-
     // queens
     selectorFunction('#d1', whiteQueen);
     selectorFunction('#d8', blackQueen);
-
     // kings
     selectorFunction('#e1', whiteKing);
     selectorFunction('#e8', blackKing);
+})
+
+
+// clear board
+$('.clearButton').click(() => {
+    console.log('clear');
+    // ----------------add this functionality----------------
 })
