c804c4b1c436a9d40fbb6d62855a3f256a1c5196
Joonas <joonas.suonpera@gmail.com>
2019-05-28 14:00:30 +0300

    testmethod works with delay

--- functionality/start.js
+++ functionality/start.js
@@ -1,15 +1,15 @@
 const whitePawn = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/800px-Chess_plt45.svg.png);'
 const whiteRook = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/68px-Chess_rlt45.svg.png);'
-const whiteKnight ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/68px-Chess_nlt45.svg.png);'
-const whiteBishop ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png);'
-const whiteQueen ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/68px-Chess_qlt45.svg.png);'
-const whiteKing ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/68px-Chess_klt45.svg.png);'
+const whiteKnight = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/68px-Chess_nlt45.svg.png);'
+const whiteBishop = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png);'
+const whiteQueen = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/68px-Chess_qlt45.svg.png);'
+const whiteKing = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/68px-Chess_klt45.svg.png);'
 const blackPawn = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/800px-Chess_pdt45.svg.png);'
-const blackRook ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png);'
-const blackKnight ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png);'
-const blackBishop ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/68px-Chess_bdt45.svg.png);'
-const blackQueen ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/68px-Chess_qdt45.svg.png);'
-const blackKing ='background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/68px-Chess_kdt45.svg.png);'
+const blackRook = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png);'
+const blackKnight = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png);'
+const blackBishop = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/68px-Chess_bdt45.svg.png);'
+const blackQueen = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/68px-Chess_qdt45.svg.png);'
+const blackKing = 'background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/68px-Chess_kdt45.svg.png);'
 
 
 
@@ -20,13 +20,12 @@ function selectorFunction(id, piece) {
     document.querySelector(id).style.cssText = piece;
 }
 
-
 // populate board when startButton is clicked
 $('.startButton').click(() => {
     // pawns
-    for(let i = 97; i < 105; i++) {
-        document.querySelector('#'+ String.fromCharCode(i) + 2).style.cssText = whitePawn;
-        document.querySelector('#'+ String.fromCharCode(i) + 7).style.cssText = blackPawn;
+    for (let i = 97; i < 105; i++) {
+        document.querySelector('#' + String.fromCharCode(i) + 2).style.cssText = whitePawn;
+        document.querySelector('#' + String.fromCharCode(i) + 7).style.cssText = blackPawn;
     }
     // rooks
     selectorFunction('#a1', whiteRook);
@@ -51,14 +50,10 @@ $('.startButton').click(() => {
     selectorFunction('#e8', blackKing);
 })
 
-
 // clear board
 $('.clearButton').click(() => {
-    console.log('clear');
-    // ----------------add this functionality----------------
+    $('div > div').css('background-image', 'none');
 })
-
-
 
 
 
@@ -69,23 +64,57 @@ var activePiece;
 var isActive = false;
 var prevLocation;
 
-
-$('div').on('click', 'div > div', function () {
-    if($(this).css('background-image') !== 'none'){
-
-        if(!isActive) {
-            $(this).css('background-color', 'green');
+function makeMove(square) {
+    if (square.css('background-image') !== 'none') {
+        if (!isActive) {
+            square.css('background-color', 'green');
             isActive = true;
-            activePiece = $(this).css('background-image');
-            prevLocation = $(this).attr('id');
+            activePiece = square.css('background-image');
+            prevLocation = square.attr('id');
             console.log(prevLocation);
         }
     } else {
-        if(isActive) {
-            $(this).css('background-image', activePiece);
-            $('#'+prevLocation).css('background-color', '');
-            $('#'+prevLocation).css('background-image', '');
+        if (isActive) {
+            square.css('background-image', activePiece);
+            $('#' + prevLocation).css('background-color', '');
+            $('#' + prevLocation).css('background-image', '');
             isActive = false;
         }
     }
-})
+}
+
+$('div').on('click', 'div > div', function () {
+    makeMove($(this));
+});
+
+
+
+
+
+//-------------------------TEST METHOD------------------------------------
+
+const berlinDefence = [
+    'e2', 'e4',
+    'e7', 'e5',
+    'g1', 'f3',
+    'b8', 'c6',
+    'f1', 'b5',
+    'g8', 'f6'
+];
+
+// loop with delay
+var i = 0;
+function loop() {
+    setTimeout(() => {
+        makeMove($('#' + berlinDefence[i]));
+        i++;
+        if (i < berlinDefence.length) {
+            loop()
+        }
+    }, 1000)
+}
+
+// testbutton onclick event
+$('.testButton').click(() => {
+    loop();
+});
